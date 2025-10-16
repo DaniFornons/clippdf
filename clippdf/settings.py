1d2c23fcb2abb2f1817ce4e1cc4386900473a891
Dani Fornons <fornons@gmail.com>
2025-10-16 16:05:36 +0200

    feat(i18n): add language switching (EN/ES/CA)

--- clippdf/settings.py
+++ clippdf/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware", 
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -61,6 +63,7 @@ TEMPLATES = [
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.request",
+                "django.template.context_processors.i18n",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
@@ -104,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ca"
 
 TIME_ZONE = "UTC"
 
@@ -112,6 +115,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Available languages shown to users
+LANGUAGES = [                   # NEW
+    ("en", _("English")),
+    ("es", _("Spanish")),
+    ("ca", _("Catalan")),
+]
+
+# Where .po/.mo files live
+LOCALE_PATHS = [                # NEW
+    BASE_DIR / "locale",
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
