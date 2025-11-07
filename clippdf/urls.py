d41a374b10ccf3a7a6fdd1255a20cc8705087d92
Dani Fornons <fornons@gmail.com>
2025-11-07 11:30:14 +0100

    fix(clippdf): deleted databas and admin

--- clippdf/urls.py
+++ clippdf/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("i18n/", include("django.conf.urls.i18n")),
     path("", include("core.urls")),
 ]
